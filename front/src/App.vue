64a431f32313b2c15ab04971a4215e6ecbe31c81
noob69 <113860914+NedVlor@users.noreply.github.com>
2023-07-12 19:53:49 +0000

    ref

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <router-view />
+
+{{counter}}
+<button @click="add">+</button>
+<!--<router-view />--->
+
 </template>
 
 <script setup>
-  //
+import { ref, onMounted } from 'vue'
+let counter = ref(0);  //
+function add(){
+counter.value++;
+console.log(counter.value);
+}
 </script>
