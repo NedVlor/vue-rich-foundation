813ef92cdf2fc7e4ae25a09dffb1e35ce9fb95e3
noob69 <113860914+NedVlor@users.noreply.github.com>
2023-07-17 19:02:39 +0000

    layout decomposition

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,46 +2,20 @@
   <v-card>
     <v-layout>
       <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
-
-      <v-app-bar
-        color="primary"
-        prominent
-      >
-        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-    
-        <v-toolbar-title>My files</v-toolbar-title>
-      
-        <v-spacer></v-spacer>
-
-        <v-btn variant="text" icon="mdi-magnify"></v-btn>
-      
-        <v-btn variant="text" icon="mdi-filter"></v-btn>
-      
-        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
-      </v-app-bar>
-        
-      <v-navigation-drawer
-        v-model="drawer"
-        location="bottom"
-        temporary
-      >
-        <v-list
-          :items="items"
-        ></v-list>
-      </v-navigation-drawer>
-
-      <v-main style="height: 500px;">
-
+      <div style="width:100%;">
+    <Nav/>
+    <v-main style="height: 500px;">
 	{{counter}}
 	<v-btn icon="mdi-minus-thick" variant="tonal" @click="remove"></v-btn>
 	<v-btn icon="mdi-plus-thick" variant="tonal" @click="add"></v-btn>
       </v-main>
+      <Footer/>
+      </div>
     </v-layout>
   </v-card>
-  <Footer/>
 </template>
-
 <script setup>
+import Nav from '@/components/Nav.vue'
 import Footer from '@/components/Footer.vue'
 import { ref, onMounted } from 'vue'
 const drawer= ref(false)
