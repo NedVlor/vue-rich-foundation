9d4e07e142c97670c5717dd40e3e817c99b3f1b1
noob69 <113860914+NedVlor@users.noreply.github.com>
2023-07-24 19:09:31 +0000

    clean up

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -7,41 +7,6 @@
   <hr />
   <div class="card-wrapper">
     <Card v-for="(item, i) in users" :key="i" :item="item" />
-    <v-card
-      v-for="(item, i) in users"
-      :key="item"
-      class="mx-auto"
-      max-width="344"
-    >
-      <v-img :src="item.avatar_url" height="200px" cover></v-img>
-
-      <v-card-title>
-        {{ item.login }}
-      </v-card-title>
-
-      <v-card-subtitle> {{ item.repos?.length }} repos </v-card-subtitle>
-
-      <v-card-actions>
-        <v-btn color="orange-lighten-2" variant="text"> Explore </v-btn>
-
-        <v-spacer></v-spacer>
-
-        <v-btn
-          :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
-          @click="show[i] = !show[i]"
-        ></v-btn>
-      </v-card-actions>
-
-      <v-expand-transition>
-        <div v-show="show[i]">
-          <v-divider></v-divider>
-
-          <v-card-text>
-            <div v-for="item in item.repos" :key="item">{{ item.name }}</div>
-          </v-card-text>
-        </div>
-      </v-expand-transition>
-    </v-card>
   </div>
 </template>
 
