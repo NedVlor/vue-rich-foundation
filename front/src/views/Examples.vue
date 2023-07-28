3aff7d8bdc561894355b1130f0da3c2a49777d36
noob69 <113860914+NedVlor@users.noreply.github.com>
2023-07-28 09:42:22 +0000

    add page examples

---
+++ front/src/views/Examples.vue
@@ -0,0 +1,19 @@
+<template>
+  {{ counter }}
+  <v-btn icon="mdi-minus-thick" variant="tonal" @click="remove"></v-btn>
+  <v-btn icon="mdi-plus-thick" variant="tonal" @click="add"></v-btn>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+let counter = ref(0); //
+function add() {
+  counter.value++;
+  console.log(counter.value);
+}
+function remove() {
+  counter.value--;
+  console.log(counter.value);
+}
+</script>
+<style></style>
