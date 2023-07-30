0afcf9d56458db29b916a712e7aea747be32bc53
noob69 <113860914+NedVlor@users.noreply.github.com>
2023-07-30 11:42:16 +0000

    add search component

--- front/src/components/Nav.vue
+++ front/src/components/Nav.vue
@@ -1,53 +1,71 @@
 <template>
-    <v-app-bar
-        color="primary"
-        prominent
-      >
-        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+  <v-app-bar color="primary" prominent>
+    <v-app-bar-nav-icon
+      variant="text"
+      @click.stop="drawer = !drawer"
+    ></v-app-bar-nav-icon>
 
-        <v-toolbar-title>My files</v-toolbar-title>
+    <v-toolbar-title>My files</v-toolbar-title>
 
-        <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
 
-        <v-btn variant="text" icon="mdi-magnify"></v-btn>
+    <v-text-field
+      :loading="loading"
+      density="compact"
+      label="Search templates"
+      append-inner-icon="mdi-magnify"
+      single-line
+      hide-details
+      @click:append-inner="onClick"
+    ></v-text-field>
 
-        <v-btn variant="text" icon="mdi-filter"></v-btn>
+    <v-btn variant="text" icon="mdi-magnify"></v-btn>
 
-        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
-      </v-app-bar>
+    <v-btn variant="text" icon="mdi-filter"></v-btn>
 
-      <v-navigation-drawer
-        v-model="drawer"
-        location="bottom"
-        temporary
-      >
-        <v-list
-          :items="items"
-        ></v-list>
-      </v-navigation-drawer>
+    <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+  </v-app-bar>
+
+  <v-navigation-drawer v-model="drawer" location="bottom" temporary>
+    <v-list :items="items"></v-list>
+  </v-navigation-drawer>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-const drawer= ref(false)
-const group=ref(null)
-const items=ref( [
-        {
-          title: 'Foo',
-          value: 'foo',
-        },
-        {
-          title: 'Bar',
-          value: 'bar',
-        },
-        {
-          title: 'Fizz',
-          value: 'fizz',
-        },
-        {
-          title: 'Buzz',
-          value: 'buzz',
-        },
-      ])
+import { ref, onMounted } from "vue";
+const loaded = ref(false);
+const loading = ref(false);
+function onClick() {
+  loading.value = true;
 
+  setTimeout(() => {
+    loading.value = false;
+    loaded.value = true;
+  }, 2000);
+}
+const drawer = ref(false);
+const group = ref(null);
+const items = ref([
+  {
+    title: "Foo",
+    value: "foo",
+  },
+  {
+    title: "Bar",
+    value: "bar",
+  },
+  {
+    title: "Fizz",
+    value: "fizz",
+  },
+  {
+    title: "Buzz",
+    value: "buzz",
+  },
+]);
 </script>
+<style>
+.v-input__details {
+  /*  display: none !important;*/
+}
+</style>
